7bcaeddfe5a3825697fa05a25c13acdbf7eec0d6
Stefano Falconi Caro <tepano2706@gmail.com>
2025-11-17 12:13:34 -0500

    Deleting unuseful comments

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,3 @@
-###########################
-# 📦 ECR Repositories
-###########################
 output "frontend_ecr_repository_url" {
   description = "ECR URL for the frontend image"
   value       = aws_ecr_repository.frontend_repository.repository_url
@@ -11,9 +8,6 @@ output "backend_ecr_repository_url" {
   value       = aws_ecr_repository.backend_repository.repository_url
 }
 
-###########################
-# ☸️ EKS Cluster
-###########################
 output "eks_cluster_name" {
   description = "EKS Cluster name"
   value       = aws_eks_cluster.demo_cluster.name
@@ -29,9 +23,6 @@ output "eks_cluster_oidc_provider_arn" {
   value       = aws_iam_openid_connect_provider.eks.arn
 }
 
-###########################
-# 🔐 IAM Roles
-###########################
 output "alb_controller_role_arn" {
   description = "ARN of the IAM Role for AWS Load Balancer Controller"
   value       = aws_iam_role.alb_controller.arn
@@ -42,9 +33,6 @@ output "github_actions_role_arn" {
   value       = aws_iam_role.github_actions_ecr.arn
 }
 
-###########################
-# 🌍 Useful Info
-###########################
 output "vpc_id" {
   description = "VPC used by the EKS cluster"
   value       = aws_vpc.eks_vpc.id
